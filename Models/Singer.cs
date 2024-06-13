b6b365e4d473e300e6a8f6851fe8dccc51d2fb4e
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2024-06-12 22:03:47 -0300

    First Commit

---
+++ Models/Singer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicAPI.Models
+{
+    [Table("singer_table")]
+    public class Singer
+    {
+        public Singer() {
+            
+        }
+
+        [Key]
+        public int SingerId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? SingerName { get; set;}
+
+        public ICollection<Musica>
+    
+}
